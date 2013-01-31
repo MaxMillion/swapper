1cb6aa7a09539f21b92039ef38ac244f1278312e
Jairaj Sethi <j@jairaj.org>
2013-01-31 13:13:37 -0500

    reduce page flickering on swap start

--- src/core.js
+++ src/core.js
@@ -71,9 +71,11 @@ Swapper._swapper = function (os, isNode, isInDOM, insertBefore, insertAfter, rem
 			insertBefore(elem2, elem1);
 		}
 
+		elem2.style.display = 'none';
 		setInitialTransitions(elem1, elem2);
 
 		setTimeout(function () {
+			elem2.style.display = computedStyles2.display;
 			setInitialTransforms(elem1, elem2, transition);
 
 			setTimeout(function () {
